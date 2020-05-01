7852e8cea4f05d6b391b71b3b881c4e7a9bd6f7e
Fenghu Zhang <fenzha@microsoft.com>
2020-05-02 07:36:52 +0800

    Initialize 30 days hacker rank code!

---
+++ 30days/D1/helloworld.py
@@ -0,0 +1,15 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+# Sample Input
+# 
+# Welcome to 30 Days of Code!
+# Sample Output
+# 
+# Hello, World. 
+# Welcome to 30 Days of Code!
