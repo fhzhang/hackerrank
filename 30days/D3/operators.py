6bc591921944c91413d8d076d951a5a1c1ba07d8
Fenghu Zhang <fenzha@microsoft.com>
2020-05-02 10:30:35 +0800

    update until day 4

---
+++ 30days/D3/operators.py
@@ -0,0 +1,24 @@
+# Task
+# Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+# Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+#https://www.hackerrank.com/challenges/30-operators/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    print(round(meal_cost*(tip_percent + tax_percent)/100 + meal_cost))
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
